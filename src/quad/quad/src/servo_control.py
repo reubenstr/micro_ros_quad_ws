529dce3266704762ab302d447e2ad9f0a9d5210f
Reuben <reubenstrangelove@gmail.com>
2021-09-13 09:09:55 -0400

    Update servo_control.py

--- src/quad/quad/src/servo_control.py
+++ src/quad/quad/src/servo_control.py
@@ -7,20 +7,15 @@ class ServoControl():
                    
    self.motion_servo_parameters = motion_servo_parameters
     
-   def convert_joint_angles_to_pulse_widths(joint_angles):
+   def convert_joint_angles_to_pulse_widths(self, joint_angles):
       servo_pulse_widths = np.array(12)
-      
-        # convert joint angles for linked legs into servo pulse widths               
-        # TEMP TODO LOAD PARAMS FROM FILE
-        motion_servo_parameters = {"degrees_at_center_pulse_width": [0, 45, -45, 0, 45, -45, 0, 45, -45, 0, 45, -45],
-                  "zero_degrees_pulse_width": [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500],
-                  "pulse_width_per_degree": [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]}        
-     
-        for i in range(12):
-            zero_degrees_pulse_width = self.motion_servo_parameters['zero_degrees_pulse_width'][i]
-            pulse_width_per_degree = self.motion_servo_parameters['pulse_width_per_degree'][i]
-            servo_pulse_widths[i] = joint_angles_linked_leg[i]
+
+      for i in range(12):
+          zero_degrees_pulse_width = self.motion_servo_parameters['zero_degrees_pulse_width'][i]
+          pulse_width_per_degree = self.motion_servo_parameters['pulse_width_per_degree'][i]
+
+          servo_pulse_widths[i] = joint_angles_linked_leg[i] * 180/np.pi * pulse_width_per_degree + zero_degrees_pulse_width
 
       return servo_pulse_widths
-  
+
       
