ebe1f70acc2d5d2a2f9a7a2c895c0679679548da
Reuben <reubenstrangelove@gmail.com>
2021-09-13 08:48:43 -0400

    Update servo_control.py

--- src/quad/quad/src/servo_control.py
+++ src/quad/quad/src/servo_control.py
@@ -11,6 +11,17 @@ class ServoControl():
    def convert_joint_angles_to_pulse_widths(joint_angles):
       servo_pulse_widths = np.array(12)
       
+        # convert joint angles for linked legs into servo pulse widths               
+        # TEMP TODO LOAD PARAMS FROM FILE
+        motion_servo_parameters = {"degrees_at_center_pulse_width": [0, 45, -45, 0, 45, -45, 0, 45, -45, 0, 45, -45],
+                  "zero_degrees_pulse_width": [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500],
+                  "pulse_width_per_degree": [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]}        
+     
+        for i in range(12):
+            zero_degrees_pulse_width = motion_servo_parameters['zero_degrees_pulse_width'][i]
+            pulse_width_per_degree = motion_servo_parameters['pulse_width_per_degree'][i]
+            servo_pulse_widths[i] = joint_angles_linked_leg[i]
+
       return servo_pulse_widths
   
       
