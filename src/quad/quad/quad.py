c9221161b7ebf6bf5e32a6edc0e8a55a71549dfd
Reuben <reubenstrangelove@gmail.com>
2021-09-18 20:45:24 -0400

    gen back

--- src/quad/quad/quad.py
+++ src/quad/quad/quad.py
@@ -125,7 +125,7 @@ def main(args=None):
         servo_pulse_widths, joint_angles, joint_angles_linked_leg = quad_commander.tick(
             motion_parameters)
         quad_publisher.set_servo_pulse_widths(servo_pulse_widths)
-           
+        """  
         temp = temp + 1
         if temp > 5:
             temp = 0
@@ -140,8 +140,7 @@ def main(args=None):
             rclpy.logging._root_logger.log("[L] FR-HIP [0]  : " + str(joint_angles_linked_leg[offset + 0] * 180/np.pi), LoggingSeverity.INFO)
             rclpy.logging._root_logger.log("[L] FR-ULEG [1] : " + str(joint_angles_linked_leg[offset + 1] * 180/np.pi), LoggingSeverity.INFO)
             rclpy.logging._root_logger.log("[L] FR-LLEG [2] : " + str(joint_angles_linked_leg[offset + 2] * 180/np.pi), LoggingSeverity.INFO)
-            """   """
-
+            """   
         executor.spin_once()
 
     executor.shutdown()
--- src/quad/quad/src/servo_control.py
+++ src/quad/quad/src/servo_control.py
@@ -10,7 +10,7 @@ class ServoControl():
 
     def convert_joint_angles_to_pulse_widths(self, joint_angles):
         servo_pulse_widths = np.empty(12)
-        
+
         for i in range(12):
             zero_degrees_pulse_width = self.motion_servo_parameters['zero_degrees_pulse_width'][i]
             pulse_width_per_degree = self.motion_servo_parameters['pulse_width_per_degree'][i]
@@ -24,7 +24,7 @@ class ServoControl():
                 joint_angle_degrees = min
 
              # match physical system    
-            if i in {4, 5, 10, 11}:
+            if i in {4, 5, 6, 9, 10, 11}:
                 servo_pulse_widths[i] = zero_degrees_pulse_width - joint_angle_degrees *  pulse_width_per_degree
             else:
                 servo_pulse_widths[i] = joint_angle_degrees *  pulse_width_per_degree + zero_degrees_pulse_width           
