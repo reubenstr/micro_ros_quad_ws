a4640a0c7b221b04b151db05c6778f3fcc02e42c
Reuben <reubenstrangelove@gmail.com>
2021-09-20 17:03:55 -0400

    upload missing files

---
+++ src/quad/config/readme.md
@@ -0,0 +1,4 @@
+motion_servos_parameters.yaml is generated when 
+executing motion_servos_calibration.launch.py for the first time 
+  
+the resulting yaml is stored at /quad_ws/install/quad/share/quad/config/motion_servo_parameters.yaml
---
+++ src/quad/launch/motion_servo_calibration.launch.py
@@ -0,0 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+    
+def generate_launch_description():
+    ld = LaunchDescription()
+    
+    motion_servo_parameters_path = os.path.join(
+        get_package_share_directory('quad'),
+        'config',
+        'motion_servo_parameters.yaml')  
+        
+    quad_node=Node(
+        package = 'quad',
+        #name = 'quad_node',
+        executable = 'motion_servo_calibration',
+        output='screen',  
+        parameters = [{"motion_servo_parameters_path": motion_servo_parameters_path}], 
+	# launch node in a new terminal to allow keyboard input
+	prefix = ['gnome-terminal --'],
+    )     
+     
+    micro_ros_agent_node=Node(
+        package = 'micro_ros_agent',
+        # name = 'quad_node',
+        executable = 'micro_ros_agent',
+         prefix=['stdbuf -o L'],
+        output='screen', 
+        # emulate_tty=True,  
+        arguments={'serial --dev /dev/ttyACM0'},            
+    )    
+    
+    ld.add_action(micro_ros_agent_node)
+    ld.add_action(quad_node)
+    return ld   
